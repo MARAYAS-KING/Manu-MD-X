a3a207466a5cc0b3ca827ae0384085d053b09109
MARAYAS-KING <marayasmarayas@gmail.com>
2024-12-26 18:39:48 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&rEE2xDKB#fqKVcyBIFjbyacQ9Yv_wh1cIF-6v_lX0JZvkKKwfli0",
 };
